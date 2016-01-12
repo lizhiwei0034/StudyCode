b9db77fe5c4040dc3cc05df47a0a38aaaa161d23
lizhiwei0034 <mystic.wei@gmail.com>
2016-01-12 16:58:49 +0800

    多态性

---
+++ src/com/lzw/learn/Polymorphic/Polymorphic03.java
@@ -0,0 +1,39 @@
+package com.lzw.learn.Polymorphic;
+/**
+ * instanceof关键字的使用（判断某一个对象是否被实例化）
+ * @author mysti_000
+ *
+ */
+class A{
+	public void tell1(){
+		System.out.println("A---tell1");
+	}
+	public void tell2() {
+		System.out.println("A---tell2");
+	}
+}
+class B extends A{
+	@Override
+	public void tell1() {
+		
+		System.out.println("B---tell1");
+	}
+	public void tell3(){
+		System.out.println("C----tell3");
+	}
+}
+public class Polymorphic03 {
+
+	public static void main(String[] args) {
+		A a =new A();
+		//判断a是否是实例化的
+		System.out.println(a instanceof A);//TRUE
+		System.out.println(a instanceof B);//FALSE
+		//B为A的子类
+		A b=new B();
+		System.out.println(b instanceof A);//TEUR
+		System.out.println(b instanceof B);//TRUE
+		
+	}
+
+}
