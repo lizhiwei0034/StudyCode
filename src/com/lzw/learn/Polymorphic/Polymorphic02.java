cb3f13508413a5c7ae2be73101e9d064d6489f4c
lizhiwei0034 <mystic.wei@gmail.com>
2016-01-12 16:56:36 +0800

    测试了一下update

--- src/com/lzw/learn/Polymorphic/Polymorphic02.java
+++ src/com/lzw/learn/Polymorphic/Polymorphic02.java
@@ -1,4 +1,34 @@
-test
-test
-tete
-config
+package com.lzw.learn.Polymorphic;
+//父类A1
+class A1{
+	public void tell1(){
+		System.out.println("A1---tell1");
+	}	
+}
+//子类B1继承自A1
+class B1 extends A1{
+	public static void tell2() {
+		System.out.println("B1---tell2");
+		
+	}
+}
+//子类C1继承自A1
+class C1 extends A1{
+	public static void  tell3() {
+		System.out.println("C1---tell3");
+	}
+}
+public class Polymorphic02 {
+
+	public static void main(String[] args) {
+		//这里想通过子类对象使用A1中的方法
+		say(new B1());
+		say(new C1());
+	}
+	
+	public static void say(A1 a) {
+		a.tell1();
+		
+	}
+
+}
